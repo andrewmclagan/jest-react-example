66abe7b69216e64c181abf46a98cbe8d194615cb
andrewmclagan <andrew@ethicaljobs.com.au>
2016-05-06 15:27:30 +1000

    no message

--- __mocks__/redux-form.js
+++ __mocks__/redux-form.js
@@ -1,3 +1,5 @@
-export const reduxForm =  () => Component => Component; 
-
-export default jest.fn();
+	// File: /__mocks__/redux-form.js (project root, next to /node_modules/ )
+	
+	export const reduxForm =  () => Component => Component; 
+	
+	export default jest.fn();
